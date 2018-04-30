71f2bcc5354916c345f7fc7084187dfa02e00de4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-04-30 16:20:54 +0530

    added code

---
+++ core/views.py
@@ -0,0 +1,37 @@
+
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Note , User 
+from .serializers import NoteSerializer , UserSerializer
+
+
+class getnotes(APIView):
+
+	def get(self,request,pk,format=None):
+		note_list = Note.objects.filter(user_id=pk)
+		serializer = NoteSerializer(note_list,many=True)
+		return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+
+class addnote(APIView):
+	def post(self,request,format=None):
+		serializer = NoteSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data,status=status.HTTP_201_CREATED)
+		return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
+
+
+class adduser(APIView):
+	def post(self,request,format=None):
+		serializer = UserSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data,status=status.HTTP_201_CREATED)
+		return Response(serializer.data,status=status.HTTP_404_BAD_REQUEST)
+
+
+		
